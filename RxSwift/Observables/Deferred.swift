dd2f43ec0c588461a574904236395567ff32d175
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2024-08-15 17:26:32 +0200

    Fix deferred

--- RxSwift/Observables/Deferred.swift
+++ RxSwift/Observables/Deferred.swift
@@ -16,50 +16,51 @@ public extension ObservableType {
      - returns: An observable sequence whose observers trigger an invocation of the given observable factory function.
      */
     static func deferred(_ observableFactory: @escaping () async throws -> Observable<Element>)
-        -> Observable<Element>
-    {
+        -> Observable<Element> {
         Deferred(observableFactory: observableFactory)
     }
 }
 
-private final actor DeferredSink<Source: ObservableType, Observer: ObserverType>: Sink, ObserverType where Source.Element == Observer.Element {
+private final actor DeferredSink<Source: ObservableType, Observer: ObserverType>: Sink,
+    ObserverType where Source.Element == Observer.Element {
     typealias Element = Observer.Element
     typealias Parent = Deferred<Source>
     let baseSink: BaseSink<Observer>
-    var innerDisposable: Disposable?
-    
+    let disposable = SingleAssignmentDisposable()
+
     init(observer: Observer) async {
         baseSink = BaseSink(observer: observer)
     }
 
     func run(_ c: C, _ parent: Parent) async {
+        let result: Source
         do {
-            let result = try await parent.observableFactory()
-            self.innerDisposable = await result.subscribe(c.call(), self)
-        }
-        catch let e {
+            result = try await parent.observableFactory()
+        } catch let e {
             await self.forwardOn(.error(e), c.call())
             await self.dispose()
+            return
         }
+
+        await disposable.setDisposable(result.subscribe(c.call(), self))?.dispose()
     }
 
     func on(_ event: Event<Element>, _ c: C) async {
-        await self.forwardOn(event, c.call())
+        await forwardOn(event, c.call())
 
         switch event {
         case .next:
             break
         case .error:
-            await self.dispose()
+            await dispose()
         case .completed:
-            await self.dispose()
+            await dispose()
         }
     }
-    
+
     func dispose() async {
-        if baseSink.setDisposed() {
-            await innerDisposable?.dispose()
-        }
+        baseSink.setDisposed()
+        await disposable.dispose()?.dispose()
     }
 }
 
@@ -74,8 +75,7 @@ private final class Deferred<Source: ObservableType>: Producer<Source.Element> {
     }
 
     override func run<Observer: ObserverType>(_ c: C, _ observer: Observer) async -> AsynchronousDisposable
-        where Observer.Element == Source.Element
-    {
+        where Observer.Element == Source.Element {
         let sink = await DeferredSink<Source, Observer>(observer: observer)
         await sink.run(c.call(), self)
         return sink
